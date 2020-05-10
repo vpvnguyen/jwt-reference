c97ba79cdd9dd3839b447ef24ffcd0ce8bd9a93b
Vincent Nguyen <vpvnguyen@gmail.com>
2020-05-10 15:11:28 -0700

    setup router and imported dashboard, login, and register; create isAuthenticated hook

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,68 @@
-import React from "react";
+import React, { useState } from "react";
+
+// react-router-dom
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Redirect,
+} from "react-router-dom";
+
+// components
+import Dashboard from "./components/Dashboard";
+import Login from "./components/Login";
+import Register from "./components/Register";
 
 const App = () => {
+  const [isAuthenticated, setIsAuthenticated] = useState(false);
+
+  const setAuth = (boolean) => {
+    setIsAuthenticated(boolean);
+  };
+
   return (
     <React.Fragment>
-      <div className="container">App</div>
+      <Router>
+        <div className="container">
+          <h1>App</h1>
+          <Switch>
+            {/* component props vs render props = render will not remount when it is passed to a component */}
+            <Route
+              exact
+              path="/login"
+              render={(props) =>
+                !isAuthenticated ? (
+                  <Login {...props} setAuth={setAuth} />
+                ) : (
+                  <Redirect to="/dashboard" />
+                )
+              }
+            />
+            <Route
+              exact
+              path="/register"
+              render={(props) =>
+                !isAuthenticated ? (
+                  <Register {...props} setAuth={setAuth} />
+                ) : (
+                  <Redirect to="/login" />
+                )
+              }
+            />
+            <Route
+              exact
+              path="/dashboard"
+              render={(props) =>
+                isAuthenticated ? (
+                  <Dashboard {...props} setAuth={setAuth} />
+                ) : (
+                  <Redirect to="/login" />
+                )
+              }
+            />
+          </Switch>
+        </div>
+      </Router>
     </React.Fragment>
   );
 };
