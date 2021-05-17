b9823d88317b889be63feed7b9170e055f1b6a48
Vincent Nguyen <vpvnguyen@gmail.com>
2021-05-16 19:47:41 -0700

    create items routes

---
+++ express-jwt-example/src/items/items.router.ts
@@ -0,0 +1,23 @@
+import { Router } from "express";
+import * as ItemsController from "./items.controller";
+
+export const itemsRouter = Router();
+
+// GET items
+itemsRouter.get("/", ItemsController.findAllItems);
+
+// GET items/:id
+itemsRouter.get("/:id", ItemsController.findItemById);
+
+// POST items
+itemsRouter.post("/", ItemsController.createItem);
+
+// PUT items/:id
+itemsRouter.put("/:id", ItemsController.updateOrCreateItemById);
+
+// DELETE items/:id
+// itemsRouter.delete("/:id", async (req: Request, res: Response) => {
+//   ItemsController.deleteItemById(req, res);
+// });
+
+itemsRouter.delete("/:id", ItemsController.deleteItemById);
