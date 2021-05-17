dfb6049f3b6e3add825854a426d62fcb9c3ab934
Vincent Nguyen <vpvnguyen@gmail.com>
2021-05-17 09:33:19 -0700

    use auth router

--- express-jwt-example/src/index.ts
+++ express-jwt-example/src/index.ts
@@ -1,24 +1,21 @@
-import * as dotenv from "dotenv";
 import express from "express";
 import cors from "cors";
 import helmet from "helmet";
+import { PORT } from "./config/env.config";
+import { authRouter } from "./auth/auth.router";
 import { itemsRouter } from "./items/items.router";
-
-dotenv.config();
-
-// check if Node.js loaded the environmental variable PORT into process.env; otherwise, exit the application
-if (!process.env.PORT) {
-  process.exit(1);
-}
-
-// parse its value as a number type and create an instance of an Express application
-const PORT: number = parseInt(process.env.PORT as string, 10);
+import { errorHandler } from "./middleware/error.middleware";
+import { notFoundHandler } from "./middleware/not-found.middleware";
 
 const app = express();
 
 app.use(helmet()); // provides you with sensible defaults such as DNS Prefetch Control, Frameguard, Hide Powered-By, HSTS, IE No Open, Don't Sniff Mimetype, and XSS Filter
 app.use(cors()); // enable all CORS requests
 app.use(express.json()); // parse incoming requests with JSON payloads, which populates the request object with a new body object containing the parsed data
+app.use(authRouter);
 app.use("/api/items", itemsRouter);
+
+app.use(errorHandler);
+app.use(notFoundHandler);
 
 app.listen(PORT, () => console.log(`Listening on port ${PORT}`));
